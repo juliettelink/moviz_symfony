60dc3fba323ada38729afc33a63afa8225e9df57
julink <linkenheld@hotmail.com>
2023-11-20 11:39:19 +0100

    partie front, affiche films

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
 use App\Repository\MovieRepository;
 use phpDocumentor\Reflection\DocBlock\Tags\Var_;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,12 +11,21 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MovieController extends AbstractController
 {
-    #[Route('/movie', name: 'app_movie')]
+    #[Route('/films', name: 'app_movies')]
     public function index(MovieRepository $movieRepository): Response
     {
         $movies = $movieRepository->findBy([],['id'=> 'DESC']);
         return $this->render('movie/index.html.twig', [
             'movies' => $movies,
+        ]);
+    }
+
+    #[Route('/films/{id}', name: 'app_movie_show')]
+    public function show(Movie $movie): response
+    {
+       // $movie = null;
+        return $this->render('movie/show.html.twig',[
+            'movie'=> $movie,
         ]);
     }
 }
